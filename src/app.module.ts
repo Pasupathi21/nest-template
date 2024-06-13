896e2b7687f61caf688128153b87933bbc52fd01
Pasupathi-M <pasupathi.m@kprinfosolution.com>
2024-06-13 23:09:43 +0530

    env file setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './config/database/database.module';
 import { TestModule } from './config/test/test.module';
 import { UtilsModule } from './utils/utils.module';
+import { ResponseModule } from './services/response/response.module';
+import { ConfigService } from '@nestjs/config'
+import { ConfigModule } from '@nestjs/config'
+import { join } from 'path'
 
 @Module({
-  imports: [DatabaseModule, TestModule, UtilsModule],
+  imports: [
+    // ************ different env setup 
+    ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: join(__dirname, '..', `.env.${process.env.NODE_ENV}`)
+  }), 
+  DatabaseModule, TestModule, UtilsModule, ResponseModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+console.log(process.env.ENV_MESSAGE)
 bootstrap();
