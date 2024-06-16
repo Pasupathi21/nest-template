2363164c95b1df6bf159c58abe0f6ee5e7ef2eb2
Pasupathi-M <pasupathi.m@kprinfosolution.com>
2024-06-16 12:06:12 +0530

    view engine added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { join } from 'path'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // ************** cors enable for all origin
+  app.enableCors({
+    origin: "*"
+  })
+
+  // *************** view engin and view dir setup
+  app.setViewEngine('pug')
+  app.setBaseViewsDir(join(__dirname, '..', 'views'))
+
+  const port = process.env.PORT || 4002
+  console.log(`app listening on http://localhost:${port}`)
+  await app.listen(port);
 }
 console.log(process.env.ENV_MESSAGE)
 bootstrap();
